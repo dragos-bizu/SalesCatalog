11ab19a12a28a562ee181e3cca11f34ed6ede236
dragos-bizu-finetune <dragos.bizu@academicmerit.com>
2026-06-29 14:37:45 +0300

    Fix unit tests

--- ui/src/i18n/index.ts
+++ ui/src/i18n/index.ts
@@ -1,13 +1,18 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import commonRo from './ro/common.json';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import commonEn from "./en/common.json";
+import commonRo from "./ro/common.json";
 
 void i18n.use(initReactI18next).init({
-  lng: 'ro',
-  fallbackLng: 'ro',
+  lng: "en",
+  fallbackLng: "en",
   interpolation: { escapeValue: false },
+  initImmediate: false,
   resources: {
     en: {
+      translation: commonEn,
+    },
+    ro: {
       translation: commonRo,
     },
   },
