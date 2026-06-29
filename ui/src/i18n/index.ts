e76b8560d9ddea93f0bf68c17efbf99df5d96ee6
dragos-bizu-finetune <dragos.bizu@academicmerit.com>
2026-06-29 14:33:38 +0300

    Add ro translation

--- ui/src/i18n/index.ts
+++ ui/src/i18n/index.ts
@@ -1,14 +1,14 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import commonEn from "./en/common.json";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import commonRo from './ro/common.json';
 
 void i18n.use(initReactI18next).init({
-  lng: "en",
-  fallbackLng: "en",
+  lng: 'ro',
+  fallbackLng: 'ro',
   interpolation: { escapeValue: false },
   resources: {
     en: {
-      translation: commonEn,
+      translation: commonRo,
     },
   },
 });
